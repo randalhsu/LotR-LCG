c3b15c946b094ca363ad116e161a15b949a81dfe
amulet <ran.dal.aaaa@gmail.com>
2012-03-27 16:45:46 +0800

    CardsTileView use deck's name

--- LotRLCG/CardsTileView.py
+++ LotRLCG/CardsTileView.py
@@ -3,7 +3,7 @@ from Area import *
 
 class CardsTileView(Area):
     def __init__(self, deck, mainWindow, parent=None):
-        super(CardsTileView, self).__init__(name='', parent=parent)
+        super(CardsTileView, self).__init__(name=deck.name, parent=parent)
         self.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
         self.setBackgroundBrush(deck.backgroundBrush())
         self.textList = []
